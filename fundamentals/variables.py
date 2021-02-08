54eeb79dc79711764f445a68d3e8de056cf44379
Spencer White <swhit210@ford.com>
2021-02-08 15:12:31 -0500

    variables in python

---
+++ fundamentals/variables.py
@@ -0,0 +1,23 @@
+# Python Variables
+age = 27
+first_name = "Spencer"
+last_name = "White"
+is_cool = True
+
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = list(("apple", "banana", "cherry"))
+x = range(6)
+x = dict(name="Spencer", age=36)
+x = set(("apple", "banana", "cherry"))
+x = frozenset(("apple", "banana", "cherry"))
+x = bytes(5)
+x = bytearray(5)
+x = memoryview(bytes(5))
+
+# Get Data Type
+print(type(x))
+
+# Print w/ Concatenation
+print(first_name + " " + last_name)
