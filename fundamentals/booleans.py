23e1de75fc4188e3e9ffd6661fb2e94780f20429
Spencer White <swhit210@ford.com>
2021-02-08 15:34:25 -0500

    booleans with python

---
+++ fundamentals/booleans.py
@@ -0,0 +1,18 @@
+# Booleans in Python
+
+print(10 > 3)
+print(10 == 9)
+print(10 < 9)
+
+if 100 < 101:
+    print(100)
+else:
+    print(101)
+
+print(bool("Hello"))
+print(bool(15))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
