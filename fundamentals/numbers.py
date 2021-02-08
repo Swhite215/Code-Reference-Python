1dc6ad942f9c66c74703313d79d12031bfd506e7
Spencer White <swhit210@ford.com>
2021-02-08 15:12:20 -0500

    numbers in python

---
+++ fundamentals/numbers.py
@@ -0,0 +1,23 @@
+# Python Numbers
+import random
+
+# INT
+x = 1
+
+# FLOAT
+y = 2.8
+
+# COMPLEX
+z = 1j
+
+print(x)
+print(y)
+print(z)
+
+# Type Conversion
+a = float(x)
+b = int(y)
+c = complex(x)
+
+# Random Number
+print(random.randrange(1,10))
