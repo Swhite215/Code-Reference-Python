a7fc2882b7e3138e155e3263afc724447e979466
Spencer White <swhit210@ford.com>
2021-02-08 15:12:06 -0500

    examples of casting

---
+++ fundamentals/casting.py
@@ -0,0 +1,10 @@
+# Casting in Python
+
+y = int(2.8) # will be 2
+z = int("3") # will be 3
+
+a = float(1) # will be 1.0
+b = float("3") # will be 3
+
+c = str(2) # will be '2'
+d = str(3.0) # will be '3.0'
