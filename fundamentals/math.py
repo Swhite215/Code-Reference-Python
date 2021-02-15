a15b52792f2a200f0b1f3212d6da443c201fd6e1
Spencer White <swhit210@ford.com>
2021-02-15 12:00:58 -0500

    math in python

---
+++ fundamentals/math.py
@@ -0,0 +1,25 @@
+# Math in Python
+import math
+
+min = min(5, 10, 25)
+print(min)
+
+max = max(5, 10, 25)
+print(max)
+
+absolute = abs(-7.99)
+print(absolute)
+
+power = pow(4, 3)
+print(power)
+
+square_root = math.sqrt(100)
+print(square_root)
+
+ceiling = math.ceil(1.2)
+print(ceiling)
+
+floor = math.floor(1.2)
+print(floor)
+
+print(math.pi)
