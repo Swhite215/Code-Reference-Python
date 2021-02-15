a1fcc5ecc838fdecd19df538fa749bfae9fbd9ff
Spencer White <swhit210@ford.com>
2021-02-15 12:01:04 -0500

    using pip

---
+++ fundamentals/pip.py
@@ -0,0 +1,12 @@
+# Package Management in Python
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
+
+# Install a Package - pip3 install package
+# Remove a Package - pip3 uninstall package
+# List Packages - pip3 list
